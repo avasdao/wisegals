ee22884acdca048171f478675dfd65cc34289b85
Ava's DAO contributors <support@avasdao.org>
2024-09-05 03:38:52 -0400

    Add new web template.

---
+++ web/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main class="max-w-5xl mx-auto py-5 flex flex-col gap-4">
+        <h1 class="text-5xl font-medium">
+            Wise Gals
+        </h1>
+
+        <p>
+            Coming soon...
+        </p>
+    </main>
+
+    <Footer />
+</template>
