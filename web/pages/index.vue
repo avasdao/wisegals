07b831e90bf9a3afab022bec3f0b167a9573a8be
Ava's DAO contributors <support@avasdao.org>
2024-10-10 00:59:34 -0400

    Update home.

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -11,33 +11,39 @@
         <img src="@/assets/auto-bot-ani.gif" class="h-32 w-32" />
 
         <section>
-            <h2 class="text-3xl text-fuchsia-500  tracking-widest">
-                What's Coming Soon...
+            <h2 class="text-4xl text-fuchsia-500  tracking-widest">
+                Coming Soonish...
             </h2>
 
             <p class="my-3 text-xl font-light italic">
-                We working hard to make your smart contracts even SMARTER!
+                We're working very hard to make your smart contracts even SMARTER!
             </p>
 
-            <ol class="pl-10 list-decimal leading-8">
+            <ol class="pl-7 leading-9">
                 <li>
-                    Automation for UTXO smart contracts on multiple networks, including:
-                    <ul class="pl-8 list-disc">
-                        <li>Bitcoin</li>
-                        <li>Bitcoin Cash</li>
-                        <li>Nexa</li>
+                    <h3 class="text-fuchsia-700 text-lg font-medium">
+                        Automation for UTXO smart contracts on multiple networks, including:
+                    </h3>
+
+                    <ul class="pl-8 list-disc text-lg tracking-wider leading-9">
+                        <li>Bitcoin (BTC)</li>
+                        <li>Bitcoin Cash (BCH)</li>
+                        <li>Nexa (NEXA)</li>
                     </ul>
                 </li>
 
                 <li>
-                    Earn passive income as an Auto-bots managers:
-                    <ol class="pl-8 list-decimal">
+                    <h3 class="text-fuchsia-700 text-lg font-medium">
+                        Earn passive income 24x7x365 as an auto-bot manager:
+                    </h3>
+
+                    <ol class="pl-8 list-decimal text-lg tracking-wider leading-9">
                         <li><NuxtLink to="/marie" class="text-blue-500 hover:underline">
-                            Marie Chanel
+                            Marie Chanel <small>— Advanced (GPU) Math &amp; Physics</small>
                         </NuxtLink></li>
 
                         <li><NuxtLink to="/ruth" class="text-blue-500 hover:underline">
-                            Ruth Laurent
+                            Ruth Laurent <small>— Restaurateur</small>
                         </NuxtLink></li>
                     </ol>
                 </li>
